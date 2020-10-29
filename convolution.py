975c7b130eba1a67b82b5f5729466fab424d1bac
Nikita Detkov <nds-gfk@mail.ru>
2020-10-30 00:41:50 +0300

    Works fine with kernels 3x3

--- convolution.py
+++ convolution.py
@@ -1,13 +1,14 @@
 import numpy as np
 from typing import List, Tuple
+from math import floor
 
 
 def add_padding(data: List[List[float]], 
                 padding: Tuple[int, int]) -> List[List[float]]:
-    assert padding[0] >= 0 and padding[1] >= 0, 'Padding can not be lower than 0'
+    assert padding[0] >= 0 and padding[1] >= 0, 'Padding value can not be less than 0.'
     
     if not isinstance(data, np.ndarray):
-        input_data = np.array(data)
+        data = np.array(data)
 
     n, m = data.shape
     add_rows, add_cols = padding
@@ -18,34 +19,41 @@ def add_padding(data: List[List[float]],
     return data_with_padding
 
 
-def convolve(input_data: List[List[float]], kernel: List[List[float]], 
+def convolve(data: List[List[float]], kernel: List[List[float]], 
              stride: Tuple[int, int] = (1, 1), 
              dilation: Tuple[int, int] = (1, 1), 
              padding: Tuple[int, int] = (0, 0)) -> List[List[float]]:
-    if not isinstance(input_data, np.ndarray):
-        input_data = np.array(input_data)
+    assert (stride[0] >= 1 and stride[1] >= 1 and 
+            dilation[0] >= 1 and dilation[1] >= 1 and
+            padding[0] >= 0 and padding[1] >= 0), 'Parameters should be equal or greater than default values.' 
+    assert (data.shape[0] >= kernel.shape[0] and 
+            data.shape[1] >= kernel.shape[1]), 'Kernel can\'t be bigger than data in terms of shape.'
+    if not isinstance(data, np.ndarray):
+        data = np.array(data)
+    n, m = data.shape
+    data = data if list(padding) == [0, 0] else add_padding(data, padding)
+    
     if not isinstance(kernel, np.ndarray):
         kernel = np.array(kernel)
-
-    data = input_data.copy() if padding == (0, 0) else add_padding(input_data, padding)
-    n, m = data.shape
+    assert (kernel.shape[0] == kernel.shape[1]), 'Kernel must be squared.'
     k = kernel.shape
+    b = k[0]//2, k[1]//2
 
-    h_out = int((n + 2 * padding[0] - k[0] - (k[0] - 1) * (dilation[0] - 1)) / stride[0]) + 1
-    w_out = int((m + 2 * padding[1] - k[1] - (k[1] - 1) * (dilation[1] - 1)) / stride[1]) + 1
-    assert h_out > 0 and w_out > 0, 'Can\'t apply input parameters'
-    
+    h_out = floor((n + 2 * padding[0] - k[0] - (k[0] - 1) * (dilation[0] - 1)) / stride[0]) + 1
+    w_out = floor((m + 2 * padding[1] - k[1] - (k[1] - 1) * (dilation[1] - 1)) / stride[1]) + 1
+    assert h_out > 0 and w_out > 0, 'Can\'t apply input parameters, one of resulting output dimension is non-positive.'
+
+
     data_out = np.zeros((h_out, w_out))
 
     for i in range(h_out):
         for j in range(w_out):
             center_x = dilation[0] + i*stride[0]
-            indices_x = ([center_x - (k+1)*dilation[0] for k in range(k[0]//2)] + 
-                         [center_x] + [center_x + (k+1)*dilation[0] for k in range(k[0]//2)])
             center_y = dilation[1] + j*stride[1]
-            indices_y = ([center_y - (k+1)*dilation[1] for k in range(k[1]//2)] + 
-                         [center_y] + [center_y + (k+1)*dilation[1] for k in range(k[1]//2)])
             
+            indices_x = [center_x + l*dilation[0] for l in range(-b[0], b[0] + 1)]
+            indices_y = [center_y + l*dilation[1] for l in range(-b[1], b[1] + 1)]
+                        
             data_submatrix = data[indices_x, :][:, indices_y]
             prod = np.multiply(data_submatrix, kernel)
             data_out[i][j] = np.sum(prod)
--- tests.py
+++ tests.py
@@ -1,10 +1,48 @@
-import numpy as np
 import unittest
-
-from convolution import convolve
+import numpy as np
+from convolution import convolve, add_padding
 
 
 class TestConvolution(unittest.TestCase):
+    def test_paddings_shape(self, N: int = 1000):
+        for _ in range(N):
+            m_h = np.random.randint(3, 100)
+            m_w = np.random.randint(3, 100)
+            random_data = np.random.rand(m_h, m_w)
+
+            rows, cols = np.random.randint(0, 100, 2)
+            random_data_with_padding = add_padding(random_data, (rows, cols))
+            
+            self.assertEqual(random_data_with_padding.shape, (m_h + rows*2, m_w + cols*2))
+
+    def test_random_case(self, N: int = 100):
+        for _ in range(N):
+            d = np.random.randint(3, 50, 2)
+            k = np.random.choice([3])#, 5, 7, 9])
+            random_data = np.random.rand(*d)
+            random_kernel = np.random.rand(k, k)
+            for __ in range(N):
+                stride = np.random.randint(1, 5, 2)
+                dilation = np.random.randint(1, 5, 2)
+                padding = tuple(np.random.randint(0, 5, 2))
+
+                h_out = int((d[0] + 2 * padding[0] - k - (k - 1) * (dilation[0] - 1)) / stride[0]) + 1
+                w_out = int((d[1] + 2 * padding[1] - k - (k - 1) * (dilation[1] - 1)) / stride[1]) + 1
+                # print(d, k, stride, dilation, padding) 
+                if d[0] < k or d[1] < k:
+                    with self.assertRaises(AssertionError):
+                        data_conved = convolve(random_data, random_kernel, stride=stride, dilation=dilation, padding=padding)
+                elif h_out <= 0 or w_out <= 0:
+                    with self.assertRaises(AssertionError):
+                        data_conved = convolve(random_data, random_kernel, stride=stride, dilation=dilation, padding=padding)
+                else:
+                    try:
+                        data_conved = convolve(random_data, random_kernel, stride=stride, dilation=dilation, padding=padding)
+                        self.assertEqual(data_conved.shape, (h_out, w_out))
+                    except AssertionError: # for non-applicable params
+                        with self.assertRaises(AssertionError):
+                            data_conved = convolve(random_data, random_kernel, stride=stride, dilation=dilation, padding=padding)
+
     def test_regular_params(self):
         data = np.array([[0, 4, 3, 2, 0, 1, 0], 
                          [4, 3, 0, 1, 0, 1, 0],
@@ -16,30 +54,58 @@ class TestConvolution(unittest.TestCase):
                            [0, 2, 3],
                            [3, 3, 3]])
 
-        # with padding = 0, stride = 1, dilation = 1
-        result_1 = convolve(data, kernel)
-        answer_1 = np.array([[43, 43, 25, 17, 6], 
-                             [52, 44, 17, 16, 6],
-                             [30, 23, 10, 11, 6]], dtype=float)
+        # stride = 1, dilation = 1, padding = 0
+        result_110 = convolve(data, kernel)
+        answer_110 = np.array([[43, 43, 25, 17, 6], 
+                               [52, 44, 17, 16, 6],
+                               [30, 23, 10, 11, 6]])
+        
+        # stride = 1, dilation = 1, padding = 1
+        result_111 = convolve(data, kernel, padding=(1, 1))
+        answer_111 = np.array([[33, 38, 24,  7,  9,  5,  3],
+                               [41, 43, 43, 25, 17,  6,  4],
+                               [45, 52, 44, 17, 16,  6,  4],
+                               [28, 30, 23, 10, 11,  6,  4],
+                               [15, 13, 12,  4,  8,  3,  1]])
+        
+        # stride = 1, dilation = 2, padding = 0
+        result_120 = convolve(data, kernel, dilation=(2, 2))
+        answer_120 = np.array([[11, 19, 3]])
 
-        # with padding = 0, stride = 2, dilation = 1
-        result_2 = convolve(data, kernel, stride=(2, 2))
-        answer_2 = np.array([[43, 25, 6], 
-                             [30, 10, 6]], dtype=float)
+        # stride = 1, dilation = 2, padding = 1
+        result_121 = convolve(data, kernel, dilation=(2, 2), padding=(1, 1))
+        answer_121 = np.array([[27, 15, 26,  6, 11],
+                               [22, 11, 19,  3,  8],
+                               [20,  8, 14,  0,  4]])
+        
+        # stride = 2, dilation = 1, padding = 0
+        result_210 = convolve(data, kernel, stride=(2, 2))
+        answer_210 = np.array([[43, 25, 6], 
+                               [30, 10, 6]])
+        
+        # stride = 2, dilation = 1, padding = 1
+        result_211 = convolve(data, kernel, stride=(2, 2), padding=(1, 1))
+        answer_211 = np.array([[33, 24,  9,  3],
+                               [45, 44, 16,  4],
+                               [15, 12,  8,  1]])
+        
+        # stride = 2, dilation = 2, padding = 0
+        result_220 = convolve(data, kernel, stride=(2, 2), dilation=(2, 2))
+        answer_220 = np.array([[11, 3]])
 
-        # with padding = 0, stride = 1, dilation = 2
-        result_3 = convolve(data, kernel, dilation=(2, 2))
-        answer_3 = np.array([[11, 19, 3]], dtype=float)
+        # stride = 2, dilation = 2, padding = 1
+        result_221 = convolve(data, kernel, stride=(2, 2), dilation=(2, 2), padding=(1, 1))
+        answer_221 = np.array([[27, 26, 11], 
+                               [20, 14,  4]])
 
-        # with padding = 0, stride = 2, dilation = 2
-        result_4 = convolve(data, kernel, stride=(2, 2), dilation=(2, 2))
-        answer_4 = np.array([[11, 3]], dtype=float)
-
-        self.assertEqual(result_1.tolist(), answer_1.tolist())
-        self.assertEqual(result_2.tolist(), answer_2.tolist())
-        self.assertEqual(result_3.tolist(), answer_3.tolist())
-        self.assertEqual(result_4.tolist(), answer_4.tolist())
-
+        self.assertEqual(result_110.tolist(), answer_110.tolist())
+        self.assertEqual(result_111.tolist(), answer_111.tolist())
+        self.assertEqual(result_120.tolist(), answer_120.tolist())
+        self.assertEqual(result_121.tolist(), answer_121.tolist())
+        self.assertEqual(result_210.tolist(), answer_210.tolist())
+        self.assertEqual(result_211.tolist(), answer_211.tolist())
+        self.assertEqual(result_220.tolist(), answer_220.tolist())
+        self.assertEqual(result_221.tolist(), answer_221.tolist())
 
 if __name__ == '__main__':
     unittest.main()
